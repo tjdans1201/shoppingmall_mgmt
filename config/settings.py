69b865c53093a98c207512b151ba997f05bdb499
Seongmun <57758265+tjdans1201@users.noreply.github.com>
2022-09-10 17:38:05 +0900

    Feat: Modeling

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'order'
 ]
 
 MIDDLEWARE = [
--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Delivery, ContryCode, DeliveryCost
 
 # Register your models here.
+admin.site.register(Delivery)
+admin.site.register(ContryCode)
+admin.site.register(DeliveryCost)
